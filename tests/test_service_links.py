8d96aeaae44a63cf9000ffe72dfb423f05902f6f
DuongNg <nguyensusan1111@gmail.com>
2026-07-31 11:43:02 +0700

    fix metric service linking and add service link test

--- tests/test_service_links.py
+++ tests/test_service_links.py
@@ -1,71 +1,45 @@
+import pandas as pd
+
+from types import SimpleNamespace
 from src.process_module.link_telemetry import build_service_links
-from src.process_module.preprocess import preprocess
-from src.process_module.metadata_loader import load_metadata
-from src.input_module.parse_query import parse_query
-from src.schemas import RawQuery
 
 
 def test_service_links_metric():
 
+    telemetry = SimpleNamespace()
 
-    # ==========================
-    # 1. Fake query
-    # ==========================
-
-    raw_query = RawQuery(
-        issue_key="DEV-125",
-        incident_description="can not find product AAA",
-        environment="Cloud B",
-        affected_system="Search Products",
-        incident_time="2022-03-20 10:10:00"
-    )
-
-
-    parsed_query = parse_query(
-        raw_query
-    )
-
-
-    # ==========================
-    # 2. Load metadata
-    # ==========================
-
-    metadata = load_metadata(
-        "/home/ubuntu/Market/cloudbed-2/telemetry",
-        parsed_query
-    )
+    telemetry.metrics = {
+        "metric_service": pd.DataFrame(
+            {
+                "timestamp": [
+                    1647771000000
+                ],
+                "cmdb_id": [
+                    "productcatalogservice"
+                ],
+                "kpi_name": [
+                    "rr"
+                ],
+                "value": [
+                    10
+                ]
+            }
+        )
+    }
 
 
-    # ==========================
-    # 3. Preprocess
-    # ==========================
+    telemetry.logs = {}
 
-    telemetry = preprocess(
-        metadata,
-        parsed_query
-    )
+    telemetry.traces = {}
 
-
-    # ==========================
-    # 4. Build links
-    # ==========================
 
     links = build_service_links(
         telemetry
     )
 
 
-    # ==========================
-    # 5. Check
-    # ==========================
-
-    print("\n======================")
-    for service, data in links.items():
-        print(
-            service,
-            "=>",
-            data["metrics"]
-        )
+    print("\n====================")
+    print(links)
 
 
     assert (
