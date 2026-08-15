bb0418037b1d8c0845bc0ccc4150ca1ef2054b77
DuongNg <nguyensusan1111@gmail.com>
2026-08-15 20:19:21 +0700

    Fix Jira issue description format

--- src/jira/jira_client.py
+++ src/jira/jira_client.py
@@ -53,8 +53,7 @@ def create_issue(
 
     jira = connect_jira()
 
-    description = f"""
-Reporter:
+    description = f"""Reporter:
 {reporter_name}
 
 Email:
@@ -73,6 +72,24 @@ Incident Description:
 {incident_description}
 """
 
+    # Jira Cloud API v3 requires description in
+    # Atlassian Document Format (ADF).
+    description_adf = {
+        "type": "doc",
+        "version": 1,
+        "content": [
+            {
+                "type": "paragraph",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": description
+                    }
+                ]
+            }
+        ]
+    }
+
     issue_dict = {
         "project": {
             "key": jira_cfg["project_key"]
@@ -82,7 +99,7 @@ Incident Description:
             if incident_description.strip()
             else f"Incident Report - {incident_time}"
         ),
-        "description": description,
+        "description": description_adf,
         "issuetype": {
             "name": "Task"
         },
